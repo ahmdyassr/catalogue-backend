4e09152e106419955660f5ba83b5e20ac8dfc451
Ahmed Yasser <ahmdyassr@gmail.com>
2024-08-17 11:37:55 +0300

    Add product model schema

---
+++ src/models/product.model.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+
+//
+// Schema for the product 
+//
+
+// Custom field schema
+const customFieldSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	value: {
+		type: mongoose.Schema.Types.Mixed,
+		required: true
+	}
+}, { _id: false })
+
+// Product schema
+const productSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		trim: true
+	},
+	description: {
+		type: String,
+		trim: true
+	},
+	price: {
+		type: Number
+	},
+	category: {
+		type: String,
+		trim: true
+	},
+	images: [{ type: String }],
+	custom: [customFieldSchema]
+}, {
+	timestamps: true
+})
+
+// Connect to separate DB for Images
+const productsDB = mongoose.connection.useDb('products')
+const Product = productsDB.model('Product', productSchema)
+
+export default Product
